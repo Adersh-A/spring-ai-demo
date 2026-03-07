7e43f9f458a1dcac7a883ae551296432a98ba5d2
Adersh A <43125084+Adersh-A@users.noreply.github.com>
2026-03-07 23:00:51 +0530

    chat memory

--- src/main/java/com/myprojects/springai/controller/ChatController.java
+++ src/main/java/com/myprojects/springai/controller/ChatController.java
@@ -1,6 +1,9 @@
 package com.myprojects.springai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +13,12 @@ public class ChatController {
 
     private final ChatClient chatClient;
 
-    public ChatController(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+    public ChatController(ChatClient.Builder builder, ChatMemory chatMemory) {
+        this.chatClient = builder.
+                defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(chatMemory).build(),
+                        new SimpleLoggerAdvisor()).
+                build();
     }
 
     @GetMapping("/chat")
